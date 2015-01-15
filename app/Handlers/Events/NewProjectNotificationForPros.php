abc55cef21781260e999379eee887e09a6311708
Andrew Schmaltz <Drew@Andrews-MacBook-Pro.local>
2015-01-14 21:08:13 -0500

    fired

--- app/Handlers/Events/NewProjectNotificationForPros.php
+++ app/Handlers/Events/NewProjectNotificationForPros.php
@@ -45,6 +45,7 @@ class NewProjectNotificationForPros implements ShouldBeQueued {
     $body = $event->project->desc;
     $photos = [];
     $photos[0] = "http://tapquote.com/".$filename;
+    $photos[0] = "http://mainstreetmower.com/project-5.png";
 
     foreach($pros as $to)
     {
