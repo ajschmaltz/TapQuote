c398abb71dba204ae48bda9efc5b1189bbb92c16
Andrew Schmaltz <Drew@Andrews-MacBook-Pro.local>
2015-01-14 21:43:10 -0500

    formatting project card

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -3,17 +3,21 @@
 <head>
   <style type="text/css">
     body {
-      font-size: 32px;
+      font-size: 24px;
       background-color: #ccc;
       font-family: sans-serif;
       margin: 0;
     }
     #rfq {
+      padding: 25px 0;
       width: 500px;
       text-align: center;
       margin: 25px;
       background-color: #fff;
       box-shadow: 0 0 5px #000;
+    }
+    .padded {
+      padding: 0 25px;
     }
   </style>
 </head>
@@ -25,7 +29,7 @@
     @foreach(explode(',', $project->photos) as $photo)
       <img src="{{ $photo }}" />
       <div class="padded">
-        <p>{{ $project->desc }}/p>
+        <p>{{ $project->desc }}</p>
       </div>
     @endforeach
   </div>
