b961b9a87e5003b116427f0e9bf27898c7bbdae5
Andrew Schmaltz <Drew@Andrews-MacBook-Pro.local>
2015-01-16 02:13:37 -0500

    test

--- app/Handlers/Events/NewProjectNotificationForPros.php
+++ app/Handlers/Events/NewProjectNotificationForPros.php
@@ -22,8 +22,7 @@ class NewProjectNotificationForPros implements ShouldBeQueued {
 	 */
 	public function __construct(Operator $operator)
 	{
-		$this->operator = $operator;
-    $this->snappy = new Image(base_path() . '/vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64');
+
 	}
 
 	/**
@@ -35,26 +34,7 @@ class NewProjectNotificationForPros implements ShouldBeQueued {
 	public function handle(ProjectWasPosted $event)
 	{
 
-    $this->snappy->setOption('quality', 50);
-    $this->snappy->setOption('width', 500);
-    $photos = [];
 
-    foreach($event->project->photos as $photo)
-    {
-      $image = $this->snappy->getOutput('http://tapquote.com/photos/' . $photo->id);
-      $filename = pubic_path() . '/project-' . $event->project->id . '-nphoto-' . $photo->id .'.jpg';
-      File::put($filename, $image);
-      $photos[] = "http://tapquote.com/".$filename;
-    }
-
-    $from = $event->project->relay->number;
-    $pros = $event->project->pros;
-    $body = $event->project->desc;
-
-    foreach($pros as $to)
-    {
-      $this->operator->sendMMS($to->cell, $from, $body, $photos);
-    }
 	}
 
 }
