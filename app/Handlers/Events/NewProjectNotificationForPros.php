a187b61a29792adf3fa9c6c3140ed5b06dde5075
Andrew Schmaltz <Drew@Andrews-MacBook-Pro.local>
2015-01-16 03:07:54 -0500

    remove failure

--- app/Handlers/Events/NewProjectNotificationForPros.php
+++ app/Handlers/Events/NewProjectNotificationForPros.php
@@ -1,7 +1,6 @@
 <?php namespace App\Handlers\Events;
 
 use App\Events\ProjectWasPosted;
-use App\Project;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldBeQueued;
 use App\Services\Operator;
@@ -12,7 +11,9 @@ class NewProjectNotificationForPros implements ShouldBeQueued {
 
   use InteractsWithQueue;
 
+  private $operator;
 
+  private $snappy;
 
 	/**
 	 * Create the event handler.
@@ -21,7 +22,8 @@ class NewProjectNotificationForPros implements ShouldBeQueued {
 	 */
 	public function __construct(Operator $operator)
 	{
-
+		$this->operator = $operator;
+    $this->snappy = new Image(base_path() . '/vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64');
 	}
 
 	/**
@@ -33,9 +35,26 @@ class NewProjectNotificationForPros implements ShouldBeQueued {
 	public function handle(ProjectWasPosted $event)
 	{
 
-    Project::findOrFail(1000);
+    $this->snappy->setOption('quality', 50);
+    $this->snappy->setOption('width', 500);
+    $photos = [];
 
+    foreach($event->project->photos as $photo)
+    {
+      $image = $this->snappy->getOutput('http://tapquote.com/photos/' . $photo->id);
+      $filename = 'project-' . $event->project->id . '-photo-' . $photo->id .'.jpg';
+      File::put($filename, $image);
+      $photos[] = "http://tapquote.com/".$filename;
+    }
 
+    $from = $event->project->relay->number;
+    $pros = $event->project->pros;
+    $body = $event->project->desc;
+
+    foreach($pros as $to)
+    {
+      $this->operator->sendMMS($to->cell, $from, $body, $photos);
+    }
 	}
 
 }
